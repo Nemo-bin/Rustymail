b2b633fada0dd9c9864f63f18f7d235e632d2a87
Nemo-bin <ben.speeder@gmail.com>
2026-04-02 21:40:08 +0100

    single threaded

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::{
+    io::{BufReader, prelude::*},
+    net::TcpListener,
+};
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => handle_connection(stream),
+            Err(e) => eprintln!("Connection failed: {}", e),
+        }
+    }
+}
+
+fn handle_connection(stream: std::net::TcpStream) {
+    let buf_reader = BufReader::new(&stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
+
+    println!("Request: {:#?}", http_request);
 }
