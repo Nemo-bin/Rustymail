b3e6b46b917b825822e18beaf9ccde01ab8d4321
Nemo-bin <ben.speeder@gmail.com>
2026-04-02 21:52:27 +0100

    refactored send recv

--- src/main.rs
+++ src/main.rs
@@ -12,13 +12,23 @@ fn main() {
     }
 }
 
-fn handle_connection(stream: std::net::TcpStream) {
-    let buf_reader = BufReader::new(&stream);
+fn handle_connection(mut stream: std::net::TcpStream) {
+    let http_request = recv(&mut stream);
+    println!("{:#?}", http_request);
+    let response = "HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\n\r\nHello, World!";
+    send(&mut stream, response);
+}
+
+fn recv(stream: &mut std::net::TcpStream) -> Vec<String>{
+    let buf_reader = BufReader::new(stream);
     let http_request: Vec<_> = buf_reader
-        .lines()
+        .lines() 
         .map(|result| result.unwrap())
         .take_while(|line| !line.is_empty())
         .collect();
+    return http_request;
+}
 
-    println!("Request: {:#?}", http_request);
+fn send(stream: &mut std::net::TcpStream, response: &str) {
+    stream.write_all(response.as_bytes()).unwrap();
 }
